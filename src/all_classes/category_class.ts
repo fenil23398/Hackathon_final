5a3a27d8b82e708c3ca00a848ed44e21a0aa7eea
malavshah9 <mdshah9574@gmail.com>
2019-02-08 23:47:27 +0530

    my app wth all work done

--- src/all_classes/category_class.ts
+++ src/all_classes/category_class.ts
@@ -1,5 +1,5 @@
 export class category{
-    constructor(public category_id:Number,public category_name:String,public cat_img:String){
+    constructor(public category_id:Number,public category_name:String,public fk_cat_id:Number,public cat_img:String){
         
     }
 }
